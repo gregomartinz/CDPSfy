1096c8c85501018b9448c2a48a742e542b1dbff3
Gregorio Martínez Luque <Grego@MacBook-Pro-de-Gregorio.local>
2016-01-16 23:13:08 +0100

    jlklkj

--- controllers/track_controller.js
+++ controllers/track_controller.js
@@ -47,7 +47,7 @@ exports.create = function (req, res) {
 	// Esta url debe ser la correspondiente al nuevo fichero en tracks.cdpsfy.es
 	var url = '/media/' + id + '.mp3';
 	//Escribimos el archivo
-	fs.writeFile('/var/CDPSfy/public/media/' + id + '.mp3', track.buffer, function(err) {
+	fs.writeFile('/var/CDPSfy/public/media/' + name + '.mp3', track.buffer, function(err) {
 		if(err){
 			return console.log(err);
 		}
