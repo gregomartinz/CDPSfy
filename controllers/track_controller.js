027f0beafc7846bcbec47da23c6d86dcb87f21e8
Gregorio Martínez Luque <Grego@MacBook-Pro-de-Gregorio.local>
2016-01-16 17:10:51 +0100

    first commit

---
+++ controllers/track_controller.js
@@ -0,0 +1,106 @@
+var fs = require('fs');
+var track_model = require('./../models/track');
+var Client = require('node-rest-client').Client;
+var dir = ["http://10.1.2.11/tracksadd/","http://10.1.2.12/tracksadd/","http://10.1.2.13/tracksadd/","http://10.1.2.14/tracksadd/"]
+var dirDel = ["http://10.1.2.11/tracksdel/","http://10.1.2.12/tracksdel/","http://10.1.2.13/tracksdel/","http://10.1.2.14/tracksdel/"]
+var update = ["updating 10.1.2.11","updating 10.1.2.12","updating 10.1.2.13","updating 10.1.2.14"]
+
+// Devuelve una lista de las canciones disponibles y sus metadatos
+exports.list = function (req, res) {
+	var tracks = track_model.tracks;
+	res.render('tracks/index', {tracks: tracks});
+};
+
+// Devuelve la vista del formulario para subir una nueva canción
+exports.new = function (req, res) {
+	res.render('tracks/new');
+};
+
+// Devuelve la vista de reproducción de una canción.
+// El campo track.url contiene la url donde se encuentra el fichero de audio
+exports.show = function (req, res) {
+	var track = track_model.tracks[req.params.trackId];
+	track.id = req.params.trackId;
+	res.render('tracks/show', {track: track});
+};
+
+// Escribe una nueva canción en el registro de canciones.
+// TODO:
+// - Escribir en tracks.cdpsfy.es el fichero de audio contenido en req.files.track.buffer
+// - Escribir en el registro la verdadera url generada al añadir el fichero en el servidor tracks.cdpsfy.es
+exports.create = function (req, res) {
+	var track = req.files.track;
+	console.log('Nuevo fichero de audio. Datos: ', track);
+	var id = track.name.split('.')[0];
+	var name = track.originalname.split('.')[0];
+
+	// Aquí debe implementarse la escritura del fichero de audio (track.buffer) en tracks.cdpsfy.es
+	// Esta url debe ser la correspondiente al nuevo fichero en tracks.cdpsfy.es
+	var url = '/media/' + id + '.mp3';
+	fs.writeFile('/var/CDPSfy/public/media/' + id + '.mp3', track.buffer, function(err) {
+		if(err){
+			return console.log(err);
+		}
+
+	});
+	
+	client = new Client();
+
+	var args = {
+		parameters:{name: name, id: id, url: url},
+		headers:{"Content-Type": "application/json"} 
+	};
+
+	for (i in dir) {
+		client.post(i, args, function(data,response) {}.on('error', function(err){console.log('request error',err);});
+	};
+	// Escribe los metadatos de la nueva canción en el registro.
+	track_model.tracks[id] = {
+		name: name,
+		url: url
+	};
+
+	res.redirect('/tracks');
+};
+
+// Borra una canción (trackId) del registro de canciones 
+// TODO:
+// - Eliminar en tracks.cdpsfy.es el fichero de audio correspondiente a trackId
+exports.destroy = function (req, res) {
+	var trackId = req.params.trackId;
+
+	// Aquí debe implementarse el borrado del fichero de audio indetificado por trackId en tracks.cdpsfy.es
+	var filePath = '/var/CDPSfy/public/media/' + trackId + '.mp3';
+	fs.unlinkSync(filePath);
+
+	client = new Client();
+
+	var args = {
+		//data: { name: track.name },
+		parameters:{id: trackId},
+		headers:{"Content-Type": "application/json"} 
+	};
+
+	for (i in dirDel) {
+		client.post(i, args, function(data,response) {}.on('error', function(err){console.log('request error',err);});
+	};
+
+	// Borra la entrada del registro de datos
+	delete track_model.tracks[trackId];
+	res.redirect('/tracks');
+};
+
+exports.addtrack = function (req,res){
+	var name = req.query.name
+	var id = req.query.id
+	var url = req.query.url
+	track_model.tracks[id] = {
+		name: name,
+		url: url
+	};
+};
+
+exports.deltrack = function (req,res){
+	var id = req.query.id
+	delete track_model.tracks[id];
+};
---
+++ routes/index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+var multer  = require('multer');
+
+var tracks_dir = process.env.TRACKS_DIR || './media/';
+
+var trackController = require('../controllers/track_controller');
+
+router.get('/', function(req, res) {
+  res.render('index');
+});
+
+router.get('/tracks', trackController.list);
+
+router.get('/tracks/new', trackController.new);
+
+router.get('/tracks/:trackId', trackController.show);
+
+router.post('/tracks', multer({inMemory: true}), trackController.create);
+
+router.delete('/tracks/:trackId', trackController.destroy);
+
+router.post('/tracksadd',trackController.addtrack);
+
+router.delete('/tracksdel', trackController.deltrack);
+
+module.exports = router;
